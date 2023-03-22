58ad7d274b491d31bbd922079da4a6f75b868903
albertkipchirchir <albert@eldohub.co.ke>
2023-03-22 13:05:12 +0300

    post form and posts ui done

--- app.js
+++ app.js
@@ -1,132 +1,168 @@
-const express = require("express")
-const bcrypt = require("bcrypt")
-const session = require("express-session")
-const multer  = require('multer')
+const express = require("express");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const multer = require("multer");
 
-const app = express()
+const app = express();
 
-require("./services/main")
+require("./services/main");
 
-app.set("view engine", "ejs")
-app.use(express.static("public"))
-app.use(express.urlencoded({extended: false}))
-app.use(session({
-    secret: 'keyboardcat',
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "keyboardcat",
     resave: false,
-    saveUninitialized: true
-  }))
+    saveUninitialized: true,
+  })
+);
 
 //method chaining
 const db = require("mysql").createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "mixmeet"
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mixmeet",
+});
 //multer js code
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, __dirname+'/public/images/profiles')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix+"-"+file.originalname)
+  destination: function (req, file, cb) {
+    cb(null, __dirname + "/public/images/profiles");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+// momment js , Intl formatting--
+app.get("/", (req, res) => {
+  if (req.session.user) {
+    res.render("home", { user: req.session.user });
+  } else {
+    res.render("index");
+  }
+});
+
+app.get("/sign-in", (req, res) => {
+  res.render("sign-in");
+});
+
+app.get("/sign-out", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+});
+
+app.post("/sign-in", (req, res) => {
+  // confirm that email is registered
+  // compare password provided with the hash in db
+  db.query(
+    "SELECT * FROM users WHERE email = ?",
+    [req.body.email],
+    (error, results) => {
+      // handle error
+      if (results.length > 0) {
+        //proceed
+        bcrypt.compare(req.body.password, results[0].password, (err, match) => {
+          if (match) {
+            // create session
+            // console.log(results)
+            req.session.user = results[0];
+            // console.log(req.sessionID)
+            res.redirect("/");
+          } else {
+            res.render("sign-in", {
+              error: true,
+              errorMessage: "Incorrect Password",
+            });
+          }
+        });
+      } else {
+        res.render("sign-in", {
+          error: true,
+          errorMessage: "Email not registered.",
+        });
+      }
     }
-  })
-const upload = multer({ storage: storage })
-
-// momment js , Intl formatting-- 
-app.get("/", (req,res)=>{
-    if(req.session.user){
-        res.render("home", {user: req.session.user})
-    }else{
-        res.render("index")
-    }
-})
-
-app.get("/sign-in", (req,res)=>{
-    res.render("sign-in")
-})
-
-app.get("/sign-out", (req,res)=>{
-    req.session.destroy(() => {
-        res.redirect("/");
-      });
-})
-
-app.post("/sign-in", (req,res)=>{
-    // confirm that email is registered
-    // compare password provided with the hash in db
-    db.query("SELECT * FROM users WHERE email = ?",[req.body.email],(error,results)=>{
-        // handle error
-        if(results.length>0){
-            //proceed
-            bcrypt.compare(req.body.password, results[0].password, (err,match)=>{
-                if(match){
-                    // create session
-                    // console.log(results)
-                    req.session.user = results[0]
-                    // console.log(req.sessionID)
-                    res.redirect("/")
-                }else{
-                    res.render("sign-in", {error: true, errorMessage: "Incorrect Password"})
-                }
-            })
-        }else{
-            res.render("sign-in", {error: true, errorMessage: "Email not registered."})
-        }
-    })
-})
+  );
+});
 
 // reset password using OTP- emails,text messages
 // postman
 
-app.get("/sign-up", (req,res)=>{
-    res.render("sign-up")
-})
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up");
+});
 
-app.get("/all-users", (req,res)=>{
-    if(req.session.user){
-        db.query("SELECT * FROM users", (err,result)=>{
-            res.json(result)    
-        })
-    }else{
-        res.json({error: "Log in"})
-    }
-   
-})
+app.get("/all-users", (req, res) => {
+  if (req.session.user) {
+    db.query("SELECT * FROM users", (err, result) => {
+      res.json(result);
+    });
+  } else {
+    res.json({ error: "Log in" });
+  }
+});
 
-app.post("/sign-up", upload.single("image"), (req,res)=>{
-    // get data - body-parser **
-    // check if confirm pass & password match **
-    // check if email is already used/ existing ** 
-    // encrypt password / create a hash **
-    // store all details in db - insert statement
-    // console.log(req.body)
-    // console.log(req.file)
-    let fileType = req.file.mimetype.slice(req.file.mimetype.indexOf("/")+1)
-    if(req.body.password === req.body.confirm){
-        db.query("SELECT email FROM users WHERE email = ?", [req.body.email], (err, results)=>{
-            if(results.length>0){
-                // email exist in db
-                res.render("sign-up", {error: true, errorMessage: "Email already exists. Use another or login"})
-            }else{
-                bcrypt.hash(req.body.password, 4, function(err, hash){
-                    // we have access to the hashed pass as hash
-                    db.query("INSERT INTO users(username,email,password,image_link,image_type,bio) values(?,?,?,?,?,?)", [req.body.username,req.body.email,hash,req.file.filename,fileType,req.body.bio ], (error)=>{
-                        if(error){
-                            res.render("sign-up", {error: true, errorMessage: "Contact Admin, and tell them something very terrible is going on in the server"})
-                        }else{
-                            res.redirect("/sign-in") // sucessful signup
-                        }
-                    })
-                })
-            }
-        } )
-    }else{
-        res.render("sign-up", {error: true, errorMessage: "Password and confirm Password do not match"})
-    }
-})
-app.listen(3001, ()=>{
-    console.log("app running!!...")
-})
+app.post("/sign-up", upload.single("image"), (req, res) => {
+  // get data - body-parser **
+  // check if confirm pass & password match **
+  // check if email is already used/ existing **
+  // encrypt password / create a hash **
+  // store all details in db - insert statement
+  // console.log(req.body)
+  // console.log(req.file)
+  let fileType = req.file.mimetype.slice(req.file.mimetype.indexOf("/") + 1);
+  if (req.body.password === req.body.confirm) {
+    db.query(
+      "SELECT email FROM users WHERE email = ?",
+      [req.body.email],
+      (err, results) => {
+        if (results.length > 0) {
+          // email exist in db
+          res.render("sign-up", {
+            error: true,
+            errorMessage: "Email already exists. Use another or login",
+          });
+        } else {
+          bcrypt.hash(req.body.password, 4, function (err, hash) {
+            // we have access to the hashed pass as hash
+            db.query(
+              "INSERT INTO users(username,email,password,image_link,image_type,bio) values(?,?,?,?,?,?)",
+              [
+                req.body.username,
+                req.body.email,
+                hash,
+                req.file.filename,
+                fileType,
+                req.body.bio,
+              ],
+              (error) => {
+                if (error) {
+                  res.render("sign-up", {
+                    error: true,
+                    errorMessage:
+                      "Contact Admin, and tell them something very terrible is going on in the server",
+                  });
+                } else {
+                  res.redirect("/sign-in"); // sucessful signup
+                }
+              }
+            );
+          });
+        }
+      }
+    );
+  } else {
+    res.render("sign-up", {
+      error: true,
+      errorMessage: "Password and confirm Password do not match",
+    });
+  }
+});
+app.listen(3001, () => {
+  console.log("app running!!...");
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,18 @@
 document.querySelector(".current-user-nav-btn").addEventListener("click", ()=>{
     document.querySelector(".user-modal").classList.toggle("hide")
 })
+
+let newPostBtn = document.querySelector(".new-post")
+let form = document.querySelector(".new-post-form")
+
+newPostBtn.addEventListener("click", ()=>{
+    if(form.classList.contains("hide")){
+        // change text in btn
+        newPostBtn.textContent = "close"
+    }else{
+        newPostBtn.textContent = "+ new post"
+    }
+    form.classList.toggle("hide")
+})
+
+// post table in the 
